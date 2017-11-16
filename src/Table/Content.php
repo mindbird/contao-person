8f4a58548af6afef2b1ed299d2f11fa55684c327
Wusch <florian.otto@mindbird.de>
2017-11-16 14:30:34 +0100

    Correct import of classes Small improvements

--- src/Table/Content.php
+++ src/Table/Content.php
@@ -6,21 +6,34 @@ use Contao\Backend;
 use Mindbird\Contao\Person\Model\Person;
 use Mindbird\Contao\Person\Model\PersonArchive;
 
+/**
+ * Class Content
+ * @package Mindbird\Contao\Person\Table
+ */
 class Content extends Backend
 {
+    /**
+     * Return all template files of a particular group as array
+     * @return array
+     */
     public function getPersonTemplates()
     {
         return $this->getTemplateGroup('ce_person');
     }
 
-    public function getPersonList() {
+    /**
+     * Returns a list of all person ordered by archive
+     * @return array
+     */
+    public function getPersonList()
+    {
         $people = Person::findAll();
         $archives = PersonArchive::findAll();
         $invertedName = true; // lastname, firstname
         $return = array();
-        foreach($archives as $archive) {
+        foreach ($archives as $archive) {
             $peopleOfArchive = array();
-            foreach($people as $person) {
+            foreach ($people as $person) {
                 if ($person->pid === $archive->id) {
                     $peopleOfArchive[(string) $person->id] = $person->getFullName($invertedName);
                 }
--- src/Table/Person.php
+++ src/Table/Person.php
@@ -2,19 +2,37 @@
 
 namespace Mindbird\Contao\Person\Table;
 
+use Contao\FilesModel;
+use Contao\Image;
+
+/**
+ * Class Person
+ * @package Mindbird\Contao\Person\Table
+ */
 class Person
 {
+    /**
+     * Returns the description of a row
+     * @param array $row DB row of tl_person
+     * @return string
+     */
     public function listPerson($row)
     {
+        $return = '';
         if ($row['image'] != null) {
-            $file = \FilesModel::findByPk(deserialize($row['image']));
+            $file = FilesModel::findByPk(deserialize($row['image']));
             $singleSRC = $file->path;
-            $sReturn = '<figure style="float: left; margin-right: 1em;"><img src="' . Image::get($singleSRC, 80, 80,
-                    'center_top') . '"></figure>';
+            $return = '<figure style="float: left; margin-right: 1em;"><img src="' .
+                Image::get($singleSRC, 80, 80, 'center_top') .
+                '"></figure>';
         }
-        $sReturn .= '<div>' . $row ['lastname'] . ', ' . $row ['firstname'] . '</div>';
+        $return .= '<div>' .
+            $row ['lastname'] .
+            ', ' .
+            $row ['firstname'] .
+            '</div>';
 
-        return $sReturn;
+        return $return;
     }
 
 }
