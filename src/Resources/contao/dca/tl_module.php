25d535489b89cd200ce8c1c7d9e9636606ebc949
Florian Otto <florian.otto@mindbird.de>
2021-11-02 15:14:52 +0100

    Fix tl_module person_archiv default value

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -9,7 +9,6 @@ $GLOBALS ['TL_DCA'] ['tl_module'] ['palettes'] ['person_list'] = '{title_legend}
 
 $GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['person_archiv'] = array(
     'label' => &$GLOBALS ['TL_LANG'] ['tl_module'] ['person_archiv'],
-    'default' => '',
     'exclude' => true,
     'inputType' => 'select',
     'foreignKey' => 'tl_person_archive.title',
