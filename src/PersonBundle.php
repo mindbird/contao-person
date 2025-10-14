fcb274d95bf03e321a21ed579c077a34bc761a19
Florian Otto <florian.otto@mindbird.de>
2025-10-14 12:18:52 +0200

    Update composer.json to require contao/core-bundle version 5.6 and refactor PersonBundle to extend AbstractBundle

--- src/PersonBundle.php
+++ src/PersonBundle.php
@@ -2,8 +2,14 @@
 
 namespace Mindbird\Contao\Person;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class PersonBundle extends Bundle
+class PersonBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
